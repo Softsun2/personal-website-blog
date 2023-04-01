89d352e57de2f409b02daf1112c43860e915b2b5
softsun2 <peyton.okubo13@gmail.com>
2023-03-31 19:42:04 -0500

    column layout

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,59 +1,38 @@
 import s from "./Portfolio.module.css";
+import { classNames } from "../../util/util";
 
-/*
-Portfolio Element:
-  Category (education/experience/projects etc..)
-  Title
-  Timeline
-  Logo
-  Content
-    description
-    media (screenshots/evidence/photos/links etc)
-  Technologies
-*/
-
-/*
-Portfolio Page:
-  Name
-  Headshot
-  Contact Info
-  About Me
-  Art/Image
-  Content
-*/
 export default function Portfolio() {
   return (
     <div>
-      <header className={s.portfolioHeader}>
-        {/* <img className={s.headShot} src={headShot} alt="behind-cleary" /> */}
-        <div className={s.contactInfo}>
-          <span>6128609250</span>
-          <span>
-            <a
-              href="https://github.com/Softsun2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              github
-            </a>
-          </span>
-          <span>
-            <a href="mailto:okubo012@umn.edu" target="_blank" rel="noreferrer">
-              okubo012@umn.edu
-            </a>
-          </span>
-        </div>
-        <p>
-          The condensed version of this shpeal is just my <a>resume</a>.
-        </p>
-        <p>
-          My name is Peyton Okubo. This is where I'm informally displaying my
-          human experience. Experience that is relevant to the community and
-          industry I’m eager to be a part of as a recent computer science
-          graduate.
-        </p>
-      </header>
-      <div>Portfolio Element</div>
+      <p className={s.tldr}>
+        The condensed version of this shpeal is just my{" "}
+        <a href="../../media/master-Peyton-Okubo.pdf">resume</a>.
+      </p>
+      <p className={classNames(s.aboutMe, s.columns)}>
+        My name is Peyton Okubo. This is where I'm informally displaying my
+        human experience. Experience that is relevant to the community and
+        industry I’m eager to be a part of as a recent computer science
+        graduate.
+      </p>
+      <p className={s.contactInfo}>
+        <span>6128609250</span>
+        {" | "}
+        <span>
+          <a
+            href="https://github.com/Softsun2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+          </a>
+        </span>
+        {" | "}
+        <span>
+          <a href="mailto:okubo012@umn.edu" target="_blank" rel="noreferrer">
+            okubo012@umn.edu
+          </a>
+        </span>
+      </p>
     </div>
   );
 }
